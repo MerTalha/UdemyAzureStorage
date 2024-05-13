9840c3a0bc665a96bc0f29ac670dd4ebfb5cd573
Mert Şimşek <merttalhasimsek@gmail.com>
2024-05-13 10:23:57 +0300

    Class Library Update

---
+++ AzureStorageLibrary/Models/Product.cs
@@ -0,0 +1,22 @@
+﻿using Azure;
+using Azure.Data.Tables;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AzureStorageLibrary.Models
+{
+    public class Product : ITableEntity
+    {
+        public string Name { get; set; }
+        public double Price { get; set; }
+        public int Stock { get; set; }
+        public string Color { get; set; }
+        public string PartitionKey { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public string RowKey { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public DateTimeOffset? Timestamp { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public ETag ETag { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+    }
+}
