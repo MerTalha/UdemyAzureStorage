d7d5d260cdbb0063bfc3c64eb38a30b54babc901
Mert Şimşek <merttalhasimsek@gmail.com>
2024-05-27 10:19:32 +0300

    Azure Function Update

--- WatermarkProcessFunction/Function1.cs
+++ WatermarkProcessFunction/Function1.cs
@@ -16,7 +16,7 @@ namespace WatermarkProcessFunction
     public class Function1
     {
         [FunctionName("Function1")]
-        public async static Task Run([QueueTrigger("watermarkqueue", Connection = "QueueConnection")]PictureWatermarkQueue myQueueItem, ILogger log)
+        public static async Task Run([QueueTrigger("watermarkqueue", Connection = "QueueConnection")]PictureWatermarkQueue myQueueItem, ILogger log)
         {
             ConnectionStrings.AzureStorageConnectionString = "AccountName=devstoreaccount1;AccountKey=Eby8vdM02xNOcqFlqUwJPLlmEtlCDXJ1OUzFT50uSRZ6IFsuFq2UVErCz4I6tq/K1SZFPTOtr/KBHBeksoGMGw==;DefaultEndpointsProtocol=http;BlobEndpoint=http://127.0.0.1:10000/devstoreaccount1;QueueEndpoint=http://127.0.0.1:10001/devstoreaccount1;TableEndpoint=http://127.0.0.1:10002/devstoreaccount1;";
 
@@ -30,7 +30,7 @@ namespace WatermarkProcessFunction
 
                 using var memoryStream = AddWatermark(myQueueItem.WatermarkText,stream);
 
-                await blobStorage.UploadAsync(memoryStream, item, EContainerName.watermarkpictures);
+                await blobStorage.UploadAsync(memoryStream, myQueueItem.WatermarkText + " " + item , EContainerName.watermarkpictures);
 
                 log.LogInformation($"{item} resmine watermark ekleme iþlemi yapýlmýþtýr.");
             }
